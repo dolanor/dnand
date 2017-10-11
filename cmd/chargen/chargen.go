8f337f141cbbbcf71f02f6c1b4ebf9632fad064c
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2017-10-12 00:52:15 +0200

    Implement basic fight
    Which is a fist fight without handling temporary damages

--- cmd/chargen/chargen.go
+++ cmd/chargen/chargen.go
@@ -15,12 +15,28 @@ func (c Ability) Bonus() int {
 type Character struct {
 	Name                              string
 	Strength, Dexterity, Constitution Ability
+	HP                                int
 }
 
+func (c Character) AC() int {
+	return 10 + c.Dexterity.Bonus()
+}
+
+func (c Character) Attack(o Character) Character {
+	roll := d20(1)
+	bab := c.Strength.Bonus()
+	if roll+bab < o.AC() {
+		fmt.Printf("%s didn't get hit\n", o.Name)
+		return o
 	}
+
+	dmg := d4(1)
+	o.HP -= dmg
+	fmt.Printf("%s lost %d HP\n", o.Name, dmg)
+	return o
 }
 
-func RoundInit(c1, c2 Character) []Character {
+func RoundOrder(c1, c2 Character) []Character {
 	i1 := d20(1) + c1.Dexterity.Bonus()
 	i2 := d20(1) + c2.Dexterity.Bonus()
 	if i1 > i2 {
@@ -31,13 +47,30 @@ func RoundInit(c1, c2 Character) []Character {
 }
 
 func Fight(c1, c2 Character) Character {
+	ro := RoundOrder(c1, c2)
 
-	for {
-
-		return c1
+	i := 1
+	for ; ; i++ {
+		fmt.Printf("Round %d\n", i)
+		for o, c := range ro {
+			fmt.Printf("%s's turn… [%d HP]\n", c.Name, c.HP)
+			var tgt *Character
+			if o == 0 {
+				tgt = &ro[1]
+			} else {
+				tgt = &ro[0]
+			}
+			*tgt = c.Attack(*tgt)
+			if tgt.HP < 1 {
+				return c
+			}
+		}
 	}
 }
 
 func main() {
 	fmt.Println(d20(3))
+	winner := Fight(Character{Name: "Corum", HP: d10(1), Strength: 10, Dexterity: 10, Constitution: 10},
+		Character{Name: "Dwalin", HP: d10(1), Strength: 12, Dexterity: 8, Constitution: 12})
+	fmt.Println("Winner is", winner.Name)
 }
