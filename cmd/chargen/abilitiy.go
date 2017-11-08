fc8d98b27cc538c031705ed085bed0f8fcdb8f61
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2017-11-08 22:00:56 +0100

    Add ability index

--- cmd/chargen/abilitiy.go
+++ cmd/chargen/abilitiy.go
@@ -6,6 +6,16 @@ import (
 
 type Ability int8
 
+// Index of abilities
+const (
+	Str int = iota
+	Dex
+	Con
+	Int
+	Wis
+	Cha
+)
+
 func (c Ability) Bonus() int {
 	return int(math.Floor(float64(c-10) / 2))
 
