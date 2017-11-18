a39729932a4744ec0527f1021611cf0c52fc13d4
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2017-11-18 03:22:03 +0100

    Add idea for the action system API
    Not a great design, needs more work. But it is a start.

--- cmd/chargen/action.go
+++ cmd/chargen/action.go
@@ -4,6 +4,30 @@ import (
 //	"fmt"
 )
 
+type Entity struct {
+	Target
+}
+
+type Target interface {
+	Character() Character
+}
+
+type Effects []Effect
+type Effect struct {
+}
+
+type Actionable interface {
+	Act(src Entity, tgt Target) Effects
+}
+
+type Attack struct {
+}
+
+func (a *Attack) Act(src Entity, tgt Target) Effects {
+	src.Character().Attack(tgt.Character())
+	return Effects{}
+}
+
 func (c Character) Attack(o Character) Character {
 	hitRoll := d20(1)
 	bab := c.Strength.Bonus()
@@ -20,4 +44,22 @@ func (c Character) Attack(o Character) Character {
 	o.HP -= dmg
 	//	fmt.Printf("%s lost %d HP\n", o.Name, dmg)
 	return o
+}
+
+type NewActionable interface {
+	Act() Effects
+}
+
+type MultiAttack struct {
+	Src     Entity
+	Targets []Target
+}
+
+// Usage:
+// ma:= MultiAttack{Src: Lala, Targets: []Target{George, Pierre, Paul}}
+// ma.Act()
+func (ma *MultiAttack) Act() Effects {
+	for _, t := range ma.Targets {
+		ma.Src.Character().Attack(t.Character())
+	}
 }
